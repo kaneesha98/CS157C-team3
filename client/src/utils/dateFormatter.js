b9e04a818c7e3def442b1486411fbd71b54a198f
Nathan Nguyen <70259439+nathannguyen0397@users.noreply.github.com>
2022-05-23 16:09:44 -0700

    Update dateFormatter.js

--- client/src/utils/dateFormatter.js
+++ client/src/utils/dateFormatter.js
@@ -1,4 +1,5 @@
 const dateFormatter = date => {
+    if(!date) return
     let d = new Date(date.replace(/-/g, '\/').replace(/T.+/, ''));
     let ye = new Intl.DateTimeFormat("en", { year: "numeric" }).format(d);
     let mo = new Intl.DateTimeFormat("en", { month: "2-digit" }).format(d);
